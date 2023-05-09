59a7ed6f5dbc8f24a249a065e80f4ad61bc00233
zxp1232 <zyy9913@outlook.com>
2023-05-09 17:06:59 +0800

    fix(zxp): change style

--- src/App.vue
+++ src/App.vue
@@ -16,23 +16,17 @@ import image2 from './assets/image2.jpg'
 </template>
 
 <style scoped>
-/* .background{
-    width: 100%;
-    height: 100%;
-    z-index:-1;
-    position: absolute;
-    left: 0;
-    top: 0;
-} */
 
 .front{
     width: 100%;
-    height: 100%;
+    min-height: 100%;
     z-index:1;
-    position: absolute;
+    /* position: absolute;
     background: url("./assets/image2.jpg") no-repeat;
     background-size: cover;
-    background-position: center;
+    background-position: center; */
 }
-
+:deep(.front>div){
+  all:revert !important;
+}
 </style>
